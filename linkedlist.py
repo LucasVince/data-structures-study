6d3a81444a37d3a8c385a5a4f8543bc5eb5385c7
LucasVince <lucashenriquevince1714@gmail.com>
2026-05-24 03:16:54 -0300

    fix: concerted prev bug in insert_before method

--- linkedlist.py
+++ linkedlist.py
@@ -50,6 +50,12 @@ class LinkedList:
 
         while current:
             if current.data == next_data:
+
+                if self.head and self.head.data == next_data:
+                    new_node.next = self.head
+                    self.head = new_node
+                    return
+                
                 new_node = Node(data)
 
                 new_node.next = current
@@ -104,20 +110,3 @@ class LinkedList:
             current = current.next
             
         return array
-
-ll = LinkedList()
-
-ll.append(20)
-ll.append(40)
-ll.prepend(10)
-ll.insert_after(20, 30)
-ll.append(50)
-ll.display()
-ll.delete(50)
-ll.display()
-ll.inset_before(20,15)
-ll.display()
-
-ll_array = ll.array_convert()
-print(ll_array)
-print(ll.length())
