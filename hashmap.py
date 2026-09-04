725106ffe706ef79865e4ed41f55f67e3011c7fa
LucasVince <lucashenriquevince1714@gmail.com>
2026-09-03 21:50:57 -0300

    feat: implmented post function in hashmap

--- hashmap.py
+++ hashmap.py
@@ -1,9 +1,9 @@
-class node:
-    def __init__(self, value):
+class Node:
+    def __init__(self, value, index):
         self.value = value
         self.index
 
-class hashmap:
+class HashMap:
     def __init__(self, size):
         self.size = size
         self.loadFactor = 0
@@ -20,16 +20,15 @@ class hashmap:
 
         return h
 
-    def getIndex(self, value):
-        return 0
-
     def get(self, value):
         return 0
 
     def post(self, value):
-        return 0
+        i = self.hasher(value)
+        data = Node(value, i)
+        self.bucket[i] = data
 
-hMap = hashmap(64)
+hMap = HashMap(64)
 
 print(hMap.hasher('abacate'))
 print(hMap.hasher('banana'))
